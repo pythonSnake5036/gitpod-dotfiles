325b7338ede63b3c5e334ee91affc53da0abec96
pythonSnake5036 <118955679+pythonSnake5036@users.noreply.github.com>
2024-06-19 12:27:37 -0700

    Update plugins.lua

--- home_files/.config/nvim/lua/plugins.lua
+++ home_files/.config/nvim/lua/plugins.lua
@@ -26,11 +26,11 @@ return {
 			local cmp_action = require('lsp-zero').cmp_action()
 
 			cmp.setup({
-	  			mapping = cmp.mapping.preset.insert({
+				mapping = cmp.mapping.preset.insert({
 					['<Tab>'] = cmp_action.tab_complete(),
 					['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
 					['<CR>'] = cmp.mapping.confirm({select = false}),
-	  			}),
+				}),
 			})
 		end
 	},
@@ -46,11 +46,20 @@ return {
 		tag = '0.1.6',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+			require("telescope").load_extension "file_browser"
+
 			local builtin = require("telescope.builtin")
+			vim.keymap.set('n', '<leader>ft', builtin.builtin, {})
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+			vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
 		end
+	},
+	{
+    	"nvim-telescope/telescope-file-browser.nvim",
+    	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	},
 }
